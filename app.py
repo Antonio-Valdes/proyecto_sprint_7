82cf974fac33c0b9bdd0fc9d80324a0401e409c9
Antonio Valdés <avaldes332@gmail.com>
2025-08-09 02:14:20 -0700

    Avances a app y notebook EDA para iniciar proyecto

--- app.py
+++ app.py
@@ -1,1 +1,9 @@
 import pandas as pd
+import streamlit as st
+import plotly as pt
+
+df = pd.read_csv("vehicles_us.csv")
+
+# Creando aplicación
+st.header("Aplicación proyecto Sprint 7")
+st.dataframe(df)
